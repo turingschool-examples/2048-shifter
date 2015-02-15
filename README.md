131c19f57307bf35124c7e11045b97a792f3251c
Steve Kinney <hello@stevekinney.net>
2015-02-15 10:45:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 2048 Shifter
+
+A first pass at a simple function that will shift all the values in a row to the right as well as combine matching values.
+
+This repository is related to [Turing School’s Game Time][gt] project.
+
+## Getting Started
+
+* Clone this repository.
+* Install the testing dependency using `npm install`.
+* You can run the tests by running `npm test` from the
+
+[gt]: https://github.com/turingschool/lesson_plans/blob/master/ruby_04-apis_and_scalability/gametime_project.markdown
---
+++ test/test.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var slideRight = require('../lib/slide-right');
+
+describe('slideRight', function () {
+  
+  it('should move a single element to the left', function () {
+    
+    var expectedOutput = [null, null, null, 2];
+
+    assert.deepEqual (slideRight([null, null, 2, null]), expectedOutput);
+    assert.deepEqual(slideRight([2, null, null, null]), expectedOutput);
+    assert.deepEqual(slideRight([null, null, null, 2]), expectedOutput);
+
+  });
+
+  it('should combine similar values', function () {
+    
+    var expectedOutput = [null, null, null, 4];
+
+    assert.deepEqual(slideRight([null, null, 2, 2]), expectedOutput);
+
+  });
+
+  it('should combine similar values and slide the rest', function () {
+    
+    var expectedOutput = [null, null, 8, 4];
+
+    assert.deepEqual(slideRight([null, 8, 2, 2]), expectedOutput);
+
+  });
+
+});
